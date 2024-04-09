4f66551fbd4393c866246f8ae68886a35d9e11a6
powtzulist <delasanarwin@gmail.com>
2024-04-09 13:34:41 +0800

    cleaned route typos in auth route

--- routes/auth.js
+++ routes/auth.js
@@ -2,13 +2,11 @@ import express from "express";
 import passport from "passport";
 const router = express.Router();
 
-router.get(
-  "/",
-  passport.authenticate("google", { scope: ["profile", "email"] })
-);
+router
+  .route("/")
+  .get(passport.authenticate("google", { scope: ["profile", "email"] }));
 
-router.get(
-  "/callback",
+router.route("/callback").get(
   passport.authenticate("google", {
     failureRedirect: "http://localhost:5173/",
     successRedirect: "http://localhost:5173/",
